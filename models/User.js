92675b77cdf63a80a3f7ba5e918943f82a18bca0
sweedalgharshi <sweedalgracius@gmail.com>
2023-04-25 12:30:33 +0530

    added user model and email validator

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const UserSchema = new mongoose.model({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minlength: 3,
+    maxlength: 20,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide a valid email",
+    },
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 6,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: "lastName",
+  },
+  location: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: "Mumbai IN",
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
