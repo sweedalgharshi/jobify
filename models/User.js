1a10a01c83985e751e9de303e9948c976536dceb
sweedalgharshi <sweedalgracius@gmail.com>
2023-04-27 12:33:33 +0530

    hash password

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -39,6 +40,11 @@ const UserSchema = new mongoose.Schema({
     maxlength: 20,
     default: "Mumbai IN",
   },
+});
+
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = mongoose.model("User", UserSchema);
