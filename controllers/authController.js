30bda1f723da38e5efa1ab676c5a5ac0969c4468
sweedalgharshi <sweedalgracius@gmail.com>
2023-04-27 13:11:43 +0530

    jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,20 @@ async function register(req, res) {
     throw new BadRequestError("Email already in use");
   }
   const user = await User.create({ name, email, password });
-  res.status(StatusCodes.CREATED).json({ user });
+
+  const token = user.createJWT();
+
+  res
+    .status(StatusCodes.CREATED)
+    .json({
+      user: {
+        email: user.email,
+        lastName: user.lastName,
+        location: user.location,
+        name: user.name,
+      },
+      token,
+    });
 }
 
 // LOGIN
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -25,6 +26,7 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide password"],
     minlength: 6,
+    select: false,
   },
 
   lastName: {
@@ -46,5 +48,11 @@ UserSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+};
 
 module.exports = mongoose.model("User", UserSchema);
