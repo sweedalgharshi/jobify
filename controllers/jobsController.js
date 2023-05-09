688a56215d6a9dd4583106ba8bf7f4d142b3958b
sweedalgharshi <sweedalgracius@gmail.com>
2023-05-09 16:07:18 +0530

    job model and create job controller

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,26 +1,40 @@
+const Job = require('../models/Job');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, NotFoundError } = require('../errors/index');
+
 // Create job
-function createJob(req, res) {
-  res.send("CREATE JOB");
+async function createJob(req, res) {
+  const { position, company } = req.body;
+
+  if (!position || !company) {
+    throw new BadRequestError('Please Provide All Values');
+  }
+
+  req.body.createdBy = req.user.userId;
+
+  const job = await Job.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ job });
 }
 
 // delete job
 function deleteJob(req, res) {
-  res.send("DELETE JOB");
+  res.send('DELETE JOB');
 }
 
 // get all jobs
 function getAllJobs(req, res) {
-  res.send("GET ALL JOBS");
+  res.send('GET ALL JOBS');
 }
 
 // update job
 function updateJob(req, res) {
-  res.send("UPDATE JOB");
+  res.send('UPDATE JOB');
 }
 
 // show stats
 function showStats(req, res) {
-  res.send("SHOW STATS");
+  res.send('SHOW STATS');
 }
 
 module.exports = {
---
+++ models/Job.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const JobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, 'Please provide company name'],
+      maxLength: 50,
+    },
+
+    position: {
+      type: String,
+      required: [true, 'Please provide position'],
+      maxLength: 100,
+    },
+
+    status: {
+      type: String,
+      enum: ['interview', 'declined', 'pending'],
+      default: 'pending',
+    },
+
+    jobType: {
+      type: String,
+      enum: ['full-time', 'part-time', 'remote', 'internship'],
+      default: 'full-time',
+    },
+
+    jobLocation: {
+      type: String,
+      default: 'Mumbai In',
+      required: true,
+    },
+
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Please provide user'],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Job', JobSchema);
