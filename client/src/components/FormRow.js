6eb9b7203ace7891ebd25bf0338dc493cbb2d073
sweedalgharshi <sweedalgracius@gmail.com>
2023-05-10 12:29:24 +0530

    create job - frontend

--- client/src/components/FormRow.js
+++ client/src/components/FormRow.js
@@ -1,11 +1,17 @@
 function FormRow({ type, name, value, handleChange, labelText }) {
   return (
     <div className="form-row">
-      <label htmlFor={name} className="form- label">
+      <label htmlFor={name} className="form-label">
         {labelText || name}
       </label>
 
-      <input type={type} name={name} value={value} onChange={handleChange} className="form-input" />
+      <input
+        type={type}
+        name={name}
+        value={value}
+        onChange={handleChange}
+        className="form-input"
+      />
     </div>
   );
 }
--- client/src/pages/dashboard/AddJob.js
+++ client/src/pages/dashboard/AddJob.js
@@ -1,4 +1,121 @@
+import { FormRow, Alert, FormRowSelect } from '../../components';
+import { useAppContext } from '../../context/appContext';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
+
 function AddJob() {
-  return <div>AddJob</div>;
+  const {
+    isEditing,
+    showAlert,
+    displayAlert,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+    handleChange,
+    clearValues,
+    isLoading,
+    createJob,
+  } = useAppContext();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // if (!position || !company || !jobLocation) {
+    //   displayAlert();
+    //   return;
+    // }
+
+    if (isEditing) {
+      // eventuall editjob() logic
+      return;
+    }
+    createJob();
+  };
+
+  const handleJobInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    handleChange({ name, value });
+  };
+
+  return (
+    <Wrapper>
+      <form className="form">
+        <h3>{isEditing ? 'edit job' : 'add job'}</h3>
+
+        {showAlert && <Alert />}
+
+        {/* position */}
+        <div className="form-center">
+          <FormRow
+            type="text"
+            name="position"
+            value={position}
+            handleChange={handleJobInput}
+          />
+
+          {/* company */}
+          <FormRow
+            type="text"
+            name="company"
+            value={company}
+            handleChange={handleJobInput}
+          />
+
+          {/* location */}
+          <FormRow
+            type="text"
+            labelText="job location"
+            name="jobLocation"
+            value={jobLocation}
+            handleChange={handleJobInput}
+          />
+
+          {/* Job Status */}
+          <FormRowSelect
+            name="status"
+            value={status}
+            handleChange={handleJobInput}
+            list={statusOptions}
+          />
+
+          {/* Job Type */}
+          <FormRowSelect
+            name="jobType"
+            labelText="job type"
+            value={jobType}
+            handleChange={handleJobInput}
+            list={jobTypeOptions}
+          />
+
+          {/* btn container */}
+          <div className="btn-container">
+            <button
+              className="btn btn-block submit-btn"
+              type="submit"
+              onClick={handleSubmit}
+              disabled={isLoading}
+            >
+              submit
+            </button>
+
+            <button
+              className="btn btn-block clear-btn"
+              onClick={(e) => {
+                e.preventDefault();
+                clearValues();
+              }}
+            >
+              clear
+            </button>
+          </div>
+        </div>
+      </form>
+    </Wrapper>
+  );
 }
 export default AddJob;
